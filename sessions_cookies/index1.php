9981fe9f3be95803b5f8ccf82d6776d2bb8eb279
Gustavo Souza <gustavoelia7@gmail.com>
2023-07-10 18:48:20 -0300

    sessions_cookies add: comment

--- sessions_cookies/index1.php
+++ sessions_cookies/index1.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
     setcookie('name', 'Gustavo', time() - 60*60*24, '/');
+    echo "this is supposed to throw an error message because the cookie doesn't exist anymore:<br>";
     echo $_COOKIE['name'];
 ?>
