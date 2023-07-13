0161ee645ea8dadc3781ef885b393bff102e95a7
Gustavo Souza <gustavoelia7@gmail.com>
2023-07-13 17:30:17 -0300

    forms (insert, update and delete)

--- mod_database/introduction/index.php
+++ mod_database/introduction/index.php
@@ -8,7 +8,9 @@
         $momento_registro = date('Y-m-d H:i:s');
 
         $sql = $pdo->prepare("INSERT INTO `clientes` VALUES (null, ?, ?, ?)");
-        $sql->execute(array($nome, $sobrenome, $momento_registro));
+        if($sql->execute(array($nome, $sobrenome, $momento_registro))) {
+            echo "<script>alert('Cliente inserido com sucesso!')</script>";
+        }
     }
     
     if(isset($_POST['atualizar'])) {
@@ -17,7 +19,19 @@
         $sobrenome = $_POST['sobrenome'];
         $momento_registro = date('Y-m-d H:i:s');
 
-        $sql = $pdo->prepare("UPDATE `clientes` SET nome='$nome', sobrenome='$sobrenome', momento_registro='$momento_registro' WHERE id=$id");
+        $sql = $pdo->prepare("UPDATE `clientes` SET nome='$nome', sobrenome='$sobrenome', momento_registro='$momento_registro' WHERE id=?");
+        if($sql->execute(array($id))) {
+            echo "<script>alert('Cliente atualizado com sucesso!')</script>";
+        }
+    }
+
+    if(isset($_POST['deletar'])) {
+        $id = $_POST['id'];
+
+        $sql = $pdo->prepare("DELETE FROM `clientes` WHERE id=?");
+        if($sql->execute(array($id))) {
+            echo "<script>alert('Cliente deletado com sucesso!')</script>";
+        }
     }
 ?>
 
@@ -35,8 +49,7 @@
             <h2>Cadastrar Cliente</h2>
             Nome: <br><input type="text" name="nome" required /><br><br>
             Sobrenome: <br><input type="text" name="sobrenome" required /><br><br>
-            <input type="submit" name="cadastrar" value="Cadastrar" /><br><br>
-            <?php if(isset($_POST['cadastrar'])) echo "<script>alert('Cliente inserido com sucesso!')</script>";?>
+            <input type="submit" name="cadastrar" value="Cadastrar" />
         </form>
 
         <form action="" method="post">
@@ -44,12 +57,13 @@
             ID: <br><input type="number" name="id" placeholder="Cliente que será atualizado" /><br><br>
             Nome: <br><input type="text" name="nome" required /><br><br>
             Sobrenome: <br><input type="text" name="sobrenome" required /><br><br>
-            <input type="submit" name="atualizar" value="Atualizar" /><br><br>
-            <?php
-                if(isset($_POST['atualizar'])) {
-                    if($sql->execute()) echo "<script>alert('Cliente atualizado com sucesso!')</script>";
-                }
-            ?>
+            <input type="submit" name="atualizar" value="Atualizar" />
+        </form>
+
+        <form action="" method="post">
+            <h2>Deletar Cliente</h2>
+            ID: <br><input type="number" name="id" placeholder="Cliente que será deletado" /><br><br>
+            <input type="submit" name="deletar" value="Deletar" />
         </form>
     </div>
 </body>
