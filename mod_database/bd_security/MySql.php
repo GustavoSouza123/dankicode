e72d24736747cfcbfd3307f6d141b85398203657
Gustavo Souza <gustavoelia7@gmail.com>
2023-07-19 16:22:37 -0300

    security and parameters when connecting to db

---
+++ mod_database/bd_security/MySql.php
@@ -0,0 +1,18 @@
+<?php
+    class MySql {
+        private static $pdo;
+
+        public static function connect() {
+            if(self::$pdo == null) {
+                try {
+                    self::$pdo = new PDO('mysql:host='.HOST.';dbname='.DB,USER,PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+                    self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                } catch(Exception $e) {
+                    echo "erro ao conectar banco de dados";
+                }
+            }
+
+            return self::$pdo;
+        }
+    }
+?>
