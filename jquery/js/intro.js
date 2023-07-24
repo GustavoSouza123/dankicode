10729dcac4930c262993f2ed51a61093cfeab1a7
Gustavo Souza <gustavoelia7@gmail.com>
2023-07-24 12:01:21 -0300

    intro to jquery

---
+++ jquery/js/intro.js
@@ -0,0 +1,12 @@
+$(function() {
+    alert("hello world!");
+})
+
+$(document).ready(function() {
+    alert("hello world!");
+})
+
+// executa depois que o html terminar de carregar as imagens, videos, etc
+$(window).on('load', function() {
+    alert("hello world!");
+})
