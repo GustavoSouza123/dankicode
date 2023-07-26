3bcef8f537f6d0cd20a3e630d70cf39fd72e7b7e
Gustavo Souza <gustavoelia7@gmail.com>
2023-07-26 16:04:04 -0300

    finished: html

--- website/index.php
+++ website/index.php
@@ -9,44 +9,108 @@
 </head>
 <body>
     <header>
-        <div class="logo">Logomarca</div>
-        <nav class="desktop">
-            <ul>
-                <li href="">Home</li>
-                <li href="">Sobre</li>
-                <li href="">Serviços</li>
-                <li href="">Contato</li>
-            </ul>
-        </nav>
-        <nav class="mobile">
-            <ul>
-                <li href="">Home</li>
-                <li href="">Sobre</li>
-                <li href="">Serviços</li>
-                <li href="">Contato</li>
-            </ul>
-        </nav>
+        <div class="center">
+            <div class="logo">Logomarca</div>
+            <nav class="desktop">
+                <ul>
+                    <li><a href="">Início</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Especialidades</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav>
+            <nav class="mobile">
+                <ul>
+                    <li><a href="">Início</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Especialidades</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav>
+        </div>
     </header>
 
     <section class="banner">
-        <form action="" method="">
-            <h2>Qual o seu melhor e-mail?</h2>
-            <input type="email" name="email" required />
-            <input type="submit" name="submit" value="Cadastrar" />
-        </form>
+        <div class="center">
+            <form action="" method="">
+                <h2>Qual o seu melhor e-mail?</h2>
+                <input type="email" name="email" required />
+                <input type="submit" name="submit" value="Cadastrar" />
+            </form>
+        </div>
     </section>
     
-
     <section class="about">
-        <div class="w50">
-            <h2>Gustavo Souza</h2>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        </div>
-        <div class="w50">
-            <img src="images/profile_photo.jpg" alt="Imagem do Gustavo Souza" />
+        <div class="center">
+            <div class="w50">
+                <h2>Gustavo Souza</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </div>
+            <div class="w50">
+                <img src="images/profile_photo.jpg" alt="Imagem do Gustavo Souza" />
+            </div>
         </div>
     </section>
 
+    <section class="expertise">
+        <div class="center">
+            <h2 class="title">Especialidades</h2>
+            <div class="box">
+                <img src="" alt="" /> <!-- icon -->
+                <h3>HTML5</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
+            <div class="box">
+                <img src="" alt="" /> <!-- icon -->
+                <h3>CSS</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
+            <div class="box">
+                <img src="" alt="" /> <!-- icon -->
+                <h3>JavaScript</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="services">
+        <div class="center">
+            <div class="w50">
+                <h2 class="title">Depoimentos</h2>
+                <div class="comment">
+                    <p class="text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <p class="client">Nome do cliente</p>
+                </div>
+                <div class="comment">
+                    <p class="text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <p class="client">Nome do cliente</p>
+                </div>
+                <div class="comment">
+                    <p class="text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <p class="client">Nome do cliente</p>
+                </div>
+            </div>
+            <div class="w50">
+                <h2 class="title">Serviços</h2>
+                <div class="services-list">
+                    <ul>
+                        <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</li>
+                        <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</li>
+                        <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</li>
+                        <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="center">
+            <p>Todos os direitos reservados</p>
+        </div>
+    </footer>
 </body>
 </html>
