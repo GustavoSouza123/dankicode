bd666d6d7d0f7b64dcd9710ca3c064d3532729cd
Gustavo Souza <gustavoelia7@gmail.com>
2023-07-26 15:40:33 -0300

    inicial commit for the first project

---
+++ website/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Descrição do meu website">
+    <meta name="keywords" content="palavras,chave,do,meu,website">
+    <title>Meu Website</title>
+</head>
+<body>
+    <header>
+        <div class="logo">Logomarca</div>
+        <nav class="desktop">
+            <ul>
+                <li href="">Home</li>
+                <li href="">Sobre</li>
+                <li href="">Serviços</li>
+                <li href="">Contato</li>
+            </ul>
+        </nav>
+        <nav class="mobile">
+            <ul>
+                <li href="">Home</li>
+                <li href="">Sobre</li>
+                <li href="">Serviços</li>
+                <li href="">Contato</li>
+            </ul>
+        </nav>
+    </header>
+
+    <section class="banner">
+        <form action="" method="">
+            <h2>Qual o seu melhor e-mail?</h2>
+            <input type="email" name="email" required />
+            <input type="submit" name="submit" value="Cadastrar" />
+        </form>
+    </section>
+    
+
+    <section class="about">
+        <div class="w50">
+            <h2>Gustavo Souza</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </div>
+        <div class="w50">
+            <img src="images/profile_photo.jpg" alt="Imagem do Gustavo Souza" />
+        </div>
+    </section>
+
+</body>
+</html>
