a8351b6086d10cd6463a16b1f6eb48ee275714ef
Gustavo Souza <gustavoelia7@gmail.com>
2023-08-03 18:38:28 -0300

    modal window: name verification

--- jquery/modal window/script.js
+++ jquery/modal window/script.js
@@ -1,9 +1,17 @@
 $(function() {
+    // expressões regulares
+    var frase = 'gustavoelia7@gmail.com';
+
+    // var verificacao = frase.match(/olá/);
+    // var verificacao = frase.match(/[A-Za-z0-9]/);
+    // var verificacao = frase.match(/[a-z]{2,5}/);
+    // var verificacao = frase.match(/^(.*?)@(.*?)$/);
+    // console.log(verificacao);
+
+    // modal
     var fadeTime = 200;
 
-    abrirJanela();
-    fechar();
-
+    // abrir janela ao clicar no botão principal
     function abrirJanela() {
         $('.btn').click(function(e) {
             e.stopPropagation();
@@ -13,6 +21,9 @@ $(function() {
         });
     }
 
+    abrirJanela();
+
+    // fechar janela ao clicar no body e no botão de fechar
     function fechar() {
         var el = $('body, .close');
         el.click(function() {
@@ -22,5 +33,46 @@ $(function() {
         $('form').click(function(e) {
             e.stopPropagation();
         })
+    }
+
+    fechar();
+
+    // verificações ao enviar o formulário
+    $('form').submit(function() {
+        var nome = $('input[name=nome]').val();
+        var telefone = $('input[name=telefone]').val();
+        var email = $('input[name=email]').val();
+
+        // verificações para o nome completo
+        var splitNome = nome.split(' ');
+        var nomes = nome.split(' ').length;
+        if(nomes >= 2) {
+            campoValido($('input[name=nome]'));
+            for(var i = 0; i < nomes; i++) {
+                if(splitNome[i].match(/^[A-Z]{1}[a-z]{1,}$/)) {
+                    campoValido($('input[name=nome]'))
+                } else {
+                    campoInvalido($('input[name=nome]'));
+                    return false;
+                }
+            }
+        } else {
+            campoInvalido($('input[name=nome]'));
+            return false;
+        }
+
+        return false;
+    });
+
+    // aplicar estilos quando o campo for inválido
+    function campoInvalido(el) {
+        el.addClass('error');
+        el.data('invalido', 'true');
+    }
+
+    // retirar estilos quando o campo for válido
+    function campoValido(el) {
+        el.removeClass('error');
+        el.data('invalido', 'false');
     }
 })
