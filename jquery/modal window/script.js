25f1a7e43b54a4b1f6f9d0913f8de8f17552de69
Gustavo Souza <gustavoelia7@gmail.com>
2023-08-06 20:30:14 -0300

    modal form: phone and email validations

--- jquery/modal window/script.js
+++ jquery/modal window/script.js
@@ -1,14 +1,9 @@
+/* JANELA MODAL COM JQUERY */
+
 $(function() {
-    // expressões regulares
-    var frase = 'gustavoelia7@gmail.com';
+    /* funções para abrir e fechar janela do modal */
 
-    // var verificacao = frase.match(/olá/);
-    // var verificacao = frase.match(/[A-Za-z0-9]/);
-    // var verificacao = frase.match(/[a-z]{2,5}/);
-    // var verificacao = frase.match(/^(.*?)@(.*?)$/);
-    // console.log(verificacao);
-
-    // modal
+    // tempo da animação ao abrir e fechar modal
     var fadeTime = 200;
 
     // abrir janela ao clicar no botão principal
@@ -20,57 +15,94 @@ $(function() {
             // $('.bg').hide() // hide element
         });
     }
-
-    abrirJanela();
-
+    
     // fechar janela ao clicar no body e no botão de fechar
     function fechar() {
         var el = $('body, .close');
         el.click(function() {
             $('.bg').fadeOut(fadeTime);
         });
-
+        
         $('form').click(function(e) {
             e.stopPropagation();
         })
     }
-
+    
+    abrirJanela();
     fechar();
 
-    // verificações ao enviar o formulário
+    /* validações ao enviar o formulário */
+
+    // evento submit do formulário
     $('form').submit(function() {
         var nome = $('input[name=nome]').val();
         var telefone = $('input[name=telefone]').val();
         var email = $('input[name=email]').val();
 
-        // verificações para o nome completo
+        if(validarNome(nome) == false) {
+            campoInvalido($('input[name=nome]'));
+            return false;
+        } else if(validarTelefone(telefone) == false) {
+            campoInvalido($('input[name=telefone]'));
+            return false;
+        } else if(validarEmail(email) == false) {
+            campoInvalido($('input[name=email]'));
+            return false;
+        } else {
+            alert('formulário enviado com sucesso');
+        }
+        
+    });
+
+    // validações para o nome completo
+    function validarNome(nome) {
+        if(nome == '') return false;
+
         var splitNome = nome.split(' ');
         var nomes = nome.split(' ').length;
         if(nomes >= 2) {
             campoValido($('input[name=nome]'));
             for(var i = 0; i < nomes; i++) {
-                if(splitNome[i].match(/^[A-Z]{1}[a-z]{1,}$/)) {
-                    campoValido($('input[name=nome]'))
-                } else {
-                    campoInvalido($('input[name=nome]'));
+                if(splitNome[i].match(/^[A-Z]{1}[a-z]{1,}$/) == null) {
                     return false;
+                } else {
+                    campoValido($('input[name=nome]'));
                 }
             }
         } else {
-            campoInvalido($('input[name=nome]'));
             return false;
         }
+    }
 
-        return false;
-    });
+    // validações para o telefone
+    function validarTelefone(telefone) {
+        if(telefone == '') return false;
 
-    // aplicar estilos quando o campo for inválido
+        if(telefone.match(/^\([0-9]{2}\) [0-9]{4}-[0-9]{4}$/) == null) {
+            return false;
+        } else {
+            campoValido($('input[name=telefone]'));
+        }
+    }
+
+    // validações para o e-mail
+    function validarEmail(email) {
+        if(email == '') return false;
+
+        if(email.match(/^[a-z0-9-_.]{1,}@[a-z.]{1,}$/) == null) {
+            return false;
+        } else {
+            campoValido($('input[name=email]'));
+        }
+    }
+
+    /* funções para estilizar o campo do formulário */
+
     function campoInvalido(el) {
         el.addClass('error');
         el.data('invalido', 'true');
     }
 
-    // retirar estilos quando o campo for válido
     function campoValido(el) {
         el.removeClass('error');
         el.data('invalido', 'false');
