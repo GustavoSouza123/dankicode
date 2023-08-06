7b05ab2f20f522b24732b4844865d54ef71be23f
Gustavo Souza <gustavoelia7@gmail.com>
2023-08-06 19:43:14 -0300

    regular expressions file

---
+++ jquery/basic concepts/js/regularExpressions.js
@@ -0,0 +1,10 @@
+$(function() {
+    // expressões regulares
+    var frase = 'gustavoelia7@gmail.com';
+
+    // var verificacao = frase.match(/olá/);
+    // var verificacao = frase.match(/[A-Za-z0-9]/);
+    // var verificacao = frase.match(/[a-z]{2,5}/);
+    // var verificacao = frase.match(/^(.*?)@(.*?)$/);
+    // console.log(verificacao);
+})
