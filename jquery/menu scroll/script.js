d34a162813e42c56a8c60e4b75addb3266cc03a9
Gustavo Souza <gustavoelia7@gmail.com>
2023-08-08 10:21:14 -0300

    menu animated with scrolltop

---
+++ jquery/menu scroll/script.js
@@ -0,0 +1,24 @@
+$(function() {
+    $(window).scroll(function() {
+        var windowOffY = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        $('section').each(function() {
+            var elOffY = $(this).offset().top;
+            if(elOffY + $(this).height()/2 < (windowOffY + windowHeight)) {
+                var id = $(this).attr('id');
+                $('a').css('border-bottom', '0');
+                $('.'+id).css('border-bottom', '1px solid #fff');
+                return;
+            }
+        })
+    })
+
+    $('a').click(function() {
+        var link = $(this).attr('class');
+        $('html, body').animate({
+            scrollTop: $('#'+link).offset().top-50,
+        }, 500);
+
+        return false;
+    })
+})
