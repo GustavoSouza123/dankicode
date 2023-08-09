b625d7e452bda5b9e40e233cbdec419e26d38f7e
Gustavo Souza <gustavoelia7@gmail.com>
2023-08-09 19:22:38 -0300

    finished: slider

--- jquery/slider/script.js
+++ jquery/slider/script.js
@@ -1,18 +1,65 @@
 $(function() {
     var currentId = 0;
     var maxId = $('.slider img').length;
-    var delay = 3000;
+    var delay = 5000;
+    var animation;
 
-    startSlider();
-    function startSlider() {
+    function startSlide() {
+        for(var i = 0; i < maxId; i++) {
+            $('.bullets-nav').append('<span></span>');
+        }
         $('.slider img').eq(0).fadeIn();
-        setInterval(function() { changeSlide() }, delay);
+        animation = setInterval(function() { changeSlide() }, delay);
     }
+    startSlide();
 
     function changeSlide() {
         $('.slider img').eq(currentId).fadeOut();
         currentId += 1;
         if(currentId == maxId) currentId = 0;
         $('.slider img').eq(currentId).fadeIn();
+        $('.bullets-nav span').css('background-color', '#fff');
+        $('.bullets-nav span').eq(currentId).css('background-color', '#000');
     }
+
+    function clickBullet() {
+        $('.bullets-nav span').click(function() {
+            $('.slider img').eq(currentId).fadeOut();
+            currentId = $(this).index();
+            $('.slider img').eq(currentId).fadeIn();
+            $('.bullets-nav span').css('background-color', '#fff');
+            $(this).css('background-color', '#000');
+            clearInterval(animation);
+            animation = setInterval(function() { changeSlide() }, delay);
+        })
+    }
+    clickBullet();
+
+    function previousSlide() {
+        $('.previous').click(function() {
+            $('.slider img').eq(currentId).fadeOut();
+            currentId -= 1;
+            if(currentId < 0) currentId = maxId-1;
+            $('.slider img').eq(currentId).fadeIn();
+            $('.bullets-nav span').css('background-color', '#fff');
+            $('.bullets-nav span').eq(currentId).css('background-color', '#000');
+            clearInterval(animation);
+            animation = setInterval(function() { changeSlide() }, delay);
+        })
+    }
+    previousSlide();
+
+    function nextSlide() {
+        $('.next').click(function() {
+            $('.slider img').eq(currentId).fadeOut();
+            currentId += 1;
+            if(currentId == maxId) currentId = 0;
+            $('.slider img').eq(currentId).fadeIn();
+            $('.bullets-nav span').css('background-color', '#fff');
+            $('.bullets-nav span').eq(currentId).css('background-color', '#000');
+            clearInterval(animation);
+            animation = setInterval(function() { changeSlide() }, delay);
+        })
+    }
+    nextSlide();
 })
