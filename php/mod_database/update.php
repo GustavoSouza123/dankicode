5aee8d7acc3f5203bbd9dddefb45c2f3ff7b1006
Gustavo Souza <gustavoelia7@gmail.com>
2023-08-20 17:01:07 -0300

    fix: prepared statement

--- php/mod_database/update.php
+++ php/mod_database/update.php
@@ -2,6 +2,6 @@
     $pdo = new PDO('mysql:host=localhost;dbname=mod_dankicode', 'root', '');
 
     $id = 0;
-    $sql = $pdo->prepare("UPDATE `clientes` SET nome='?', sobrenome='?' WHERE id=?");
-    $sql->execute(array()); // necessário array com os valores a ser inseridos
+    $sql = $pdo->prepare("UPDATE `clientes` SET nome=?, sobrenome=? WHERE id=?");
+    $sql->execute(array("Clóvis", "Antônio", 20)); // necessário array com os valores a ser inseridos
 ?>
