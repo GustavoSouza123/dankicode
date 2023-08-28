0a98c03a762ae89d7e836409867bcc8b693feb33
Gustavo Souza <gustavoelia7@gmail.com>
2023-08-28 20:56:41 -0300

    initial commit: contact form

---
+++ jquery/contact form/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css" />
+    <title>Formulário de Contato</title>
+</head>
+<body>
+    <div class="container">
+        <form action="" method="post">
+            <h2>Entre em contato</h2>
+            <input type="text" name="name" placeholder="Name" />
+            <input type="email" name="email" placeholder="Email" />
+            <textarea name="message" placeholder="Message" cols="30" rows="10"></textarea>
+            <input type="submit" name="submitform" value="Submit" />
+        </form>
+    </div>
+    
+    <script src="../jquery/jquery.js"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
