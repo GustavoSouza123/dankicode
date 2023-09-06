ed57aa8ec1eefc7b56c3a77671ab277c20da6a91
Gustavo Souza <gustavoelia7@gmail.com>
2023-09-06 20:43:28 -0300

    fixes: responsiveness

--- website/js/script.js
+++ website/js/script.js
@@ -1,6 +1,16 @@
 $(function() {
-    $('.menu-toggle').click(function() {
+    $('html, body').click(function() {
+        $('.menu-toggle').removeClass('active');
+        $('.mobile').slideUp(200);
+    })
+
+    $('.menu-toggle').click(function(e) {
+        e.stopPropagation();
         $(this).toggleClass('active');
         $('.mobile').slideToggle(200);
+    })
+
+    $('.mobile').click(function(e) {
+        e.stopPropagation();
     })
 })
