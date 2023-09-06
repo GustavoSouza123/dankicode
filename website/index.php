c0bdb99728a6e8249a41ca5fc6d26b44a11c259f
Gustavo Souza <gustavoelia7@gmail.com>
2023-09-06 19:43:24 -0300

    config.php: include_path

--- website/index.php
+++ website/index.php
@@ -1,3 +1,5 @@
+<?php include('config.php') ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,7 +15,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap">
 
     <!-- style.css file -->
-    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>css/style.css" />
     
     <title>Meu Website</title>
 </head>
@@ -23,11 +25,11 @@
             <div class="logo"><a href="index.php">Logomarca</a></div>
             <nav class="desktop">
                 <ul>
-                    <li><a href="">Início</a></li>
-                    <li><a href="">Sobre</a></li>
-                    <li><a href="">Especialidades</a></li>
-                    <li><a href="">Serviços</a></li>
-                    <li><a href="">Contato</a></li>
+                    <li><a href="<?php echo INCLUDE_PATH; ?>">Início</a></li>
+                    <li><a href="<?php echo INCLUDE_PATH; ?>sobre">Sobre</a></li>
+                    <li><a href="<?php echo INCLUDE_PATH; ?>especialidades">Especialidades</a></li>
+                    <li><a href="<?php echo INCLUDE_PATH; ?>servicos">Serviços</a></li>
+                    <li><a href="<?php echo INCLUDE_PATH; ?>contato">Contato</a></li>
                 </ul>
             </nav>
             <div class="menu-toggle">
@@ -36,12 +38,12 @@
                 <span></span>
             </div>
             <nav class="mobile">
-                <ul>
-                    <li><a href="">Início</a></li>
-                    <li><a href="">Sobre</a></li>
-                    <li><a href="">Especialidades</a></li>
-                    <li><a href="">Serviços</a></li>
-                    <li><a href="">Contato</a></li>
+            <ul>
+                    <li><a href="<?php echo INCLUDE_PATH; ?>">Início</a></li>
+                    <li><a href="<?php echo INCLUDE_PATH; ?>sobre">Sobre</a></li>
+                    <li><a href="<?php echo INCLUDE_PATH; ?>especialidades">Especialidades</a></li>
+                    <li><a href="<?php echo INCLUDE_PATH; ?>servicos">Serviços</a></li>
+                    <li><a href="<?php echo INCLUDE_PATH; ?>contato">Contato</a></li>
                 </ul>
             </nav>
         </div>
@@ -66,7 +68,7 @@
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
             </div>
             <div class="w50 image">
-                <img class="me" src="images/profile-photo.jpg" alt="Imagem do Gustavo Souza" />
+                <img class="me" src="<?php echo INCLUDE_PATH; ?>images/profile-photo.jpg" alt="Imagem do Gustavo Souza" />
             </div>
         </div>
     </section>
@@ -76,17 +78,17 @@
             <h2 class="title">Especialidades</h2>
             <div class="boxes">
                 <div class="w33 box">
-                    <img class="logo" src="images/html-5.png" alt="HTML5 logo" /> <!-- icon -->
+                    <img class="logo" src="<?php echo INCLUDE_PATH; ?>images/html-5.png" alt="HTML5 logo" /> <!-- icon -->
                     <h3>HTML5</h3>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                 </div>
                 <div class="w33 box">
-                    <img class="logo" src="images/css-3.png" alt="CSS3 logo" /> <!-- icon -->
+                    <img class="logo" src="<?php echo INCLUDE_PATH; ?>images/css-3.png" alt="CSS3 logo" /> <!-- icon -->
                     <h3>CSS</h3>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                 </div>
                 <div class="w33 box">
-                    <img class="logo" src="images/js.png" alt="JavaScript logo" /> <!-- icon -->
+                    <img class="logo" src="<?php echo INCLUDE_PATH; ?>images/js.png" alt="JavaScript logo" /> <!-- icon -->
                     <h3>JavaScript</h3>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                 </div>
@@ -130,7 +132,7 @@
     </footer>
 
     <!-- scripts -->
-    <script src="js/jquery.js"></script>
-    <script type="text/javascript" src="js/script.js"></script>
+    <script src="<?php echo INCLUDE_PATH; ?>js/jquery.js"></script>
+    <script src="<?php echo INCLUDE_PATH; ?>js/script.js"></script>
 </body>
 </html>
