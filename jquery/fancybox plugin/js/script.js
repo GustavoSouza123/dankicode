49747d423c67052dc388aa3fe9e2dee7a572e82f
Gustavo Souza <gustavoelia7@gmail.com>
2023-09-18 09:29:07 -0300

    fancybox feats: galleries 1 and 2 with default options

--- jquery/fancybox plugin/js/script.js
+++ jquery/fancybox plugin/js/script.js
@@ -1,3 +1,11 @@
 $(function() {
-    
+    Fancybox.bind('[data-fancybox="gallery1"]', {});
+
+    let image = ``;
+    for(var i = 1; i < 20; i++) {
+        image = `<a data-fancybox="gallery2" data-src="https://lipsum.app/id/${i}/1024x768" data-caption="Image caption"><img src="https://lipsum.app/id/${i}/200x150" /></a>`;
+        $('.gallery2 .gallery-content').append(image);
+    }
+
+    Fancybox.bind('[data-fancybox="gallery2"]', {});
 })
