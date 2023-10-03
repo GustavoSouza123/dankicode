a21ebe5d910205bc8fe335faa3f71f1a8875286e
Gustavo Souza <gustavoelia7@gmail.com>
2023-10-03 19:35:55 -0300

    installing my first dependency with composer (with .gitignore)

--- php/composer/index.php
+++ php/composer/index.php
@@ -1,11 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Composer</title>
-</head>
-<body>
-    <h1>Testando o Composer!</h1>
-</body>
-</html>
+<?php
+    require 'vendor/autoload.php';
+
+    use Carbon\Carbon;
+
+    $tomorrow = date("d/m/Y", strtotime(Carbon::now()->addDay()));
+    printf("Now is: %s<br>", Carbon::now());
+    printf("Tomorrow is: %s", $tomorrow);
+?>
